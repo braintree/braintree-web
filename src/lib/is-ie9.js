b20f15ce9fb0e872c0a8ef88ac251c5c7f0f5324
Blade Barringer <blade@crookedneighbor.com>
2019-01-09 22:25:34 -0600

    Sync up github repo with internal repo (#409)
    ### Summary
    
    [Because we only grab the files that have changed](https://github.com/braintree/braintree-web/blob/1135fe0db3f45aa782ff28cb943ece66826f798b/scripts/release#L130) to do releases, there are various files that have gone out of sync with the internal repo.
    
    A more resilient fix will be to update the release script.
    
    ### Checklist
    
    - [ ] ~~Added a changelog entry~~

--- src/lib/is-ie9.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function isIe9(userAgent) {
-  userAgent = userAgent || navigator.userAgent;
-  return userAgent.indexOf('MSIE 9') !== -1;
-};
--- src/paypal/shared/events.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var enumerate = require('../../lib/enumerate');
-
-module.exports = enumerate([
-  'BRIDGE_FRAME_READY',
-  'AUTHORIZATION_COMPLETE'
-], 'paypal:');
