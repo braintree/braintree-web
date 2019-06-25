f0c571ec9cdecdd039acd5a2e28e6c74a363379a
Blade Barringer <blade.barringer@getbraintree.com>
2019-06-25 15:27:09 +0000

    Remove ideal component from public repo

--- src/ideal/internal/translations/en_US.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  idealSelectYourBank: 'Select Your Bank',
-  idealBack: 'Back',
-  idealContinuingTo: 'Continuing to'
-};
--- src/ideal/shared/events.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var enumerate = require('../../lib/enumerate');
-
-module.exports = enumerate([
-  'BANK_CHOSEN',
-  'REDIRECT_PAGE_REACHED'
-], 'ideal:');
