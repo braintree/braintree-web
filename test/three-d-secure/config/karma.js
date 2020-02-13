0956269762e904b3f64517f88f5bafbb9e85288a
Blade Barringer <blade.barringer@getbraintree.com>
2020-02-13 17:21:50 +0000

    Remove old unused files

--- test/three-d-secure/config/karma.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var defaults = require('../../karma-config-defaults');
-
-module.exports = function (config) {
-  config.set(Object.assign({}, defaults, {
-    basePath: '../'
-  }));
-};
