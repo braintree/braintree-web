dc090c54df52fae6249b3b098f6622205337ed98
Hash Minner <e19198@eng.pdn.ac.lk>
2022-08-31 00:44:38 +0530

    improve code formate in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,44 @@
-"use strict";
+"use strict"
 
-var gulp = require("gulp");
-var del = require("del");
-var VERSION = require("./package.json").version;
+var gulp = require("gulp")
+var del = require("del")
+var VERSION = require("./package.json").version
 
-var COMPONENTS = require("./components.json");
+var COMPONENTS = require("./components.json")
 
-require("dotenv").config();
+require("dotenv").config()
 
 gulp.task("clean", function () {
-  return del(["./dist"]);
-});
+  return del(["./dist"])
+})
 
 COMPONENTS.forEach(function (component) {
-  require("./tasks/" + component);
-});
-require("./tasks/frame-service");
-require("./tasks/build");
-require("./tasks/release");
-require("./tasks/jsdoc");
-require("./tasks/test");
+  require("./tasks/" + component)
+})
+require("./tasks/frame-service")
+require("./tasks/build")
+require("./tasks/release")
+require("./tasks/jsdoc")
+require("./tasks/test")
 
 function setNpmVersion() {
-  process.env.npm_package_version = VERSION;
+  process.env.npm_package_version = VERSION
 
-  return Promise.resolve();
+  return Promise.resolve()
 }
 
 function watch() {
-  process.env.BRAINTREE_JS_ENV = "development";
+  process.env.BRAINTREE_JS_ENV = "development"
 
   COMPONENTS.forEach(function (component) {
     gulp
       .watch(["src/" + component + "/**", "src/lib/**"])
-      .on("change", gulp.series("build:" + component));
-  });
+      .on("change", gulp.series("build:" + component))
+  })
 
-  gulp.watch(["src/**/*", "jsdoc/*"], gulp.series("jsdoc"));
+  gulp.watch(["src/**/*", "jsdoc/*"], gulp.series("jsdoc"))
 }
 
-gulp.task("build:integration", gulp.series(setNpmVersion, "build", "jsdoc"));
+gulp.task("build:integration", gulp.series(setNpmVersion, "build", "jsdoc"))
 
-gulp.task("watch:integration", gulp.series(setNpmVersion, watch));
+gulp.task("watch:integration", gulp.series(setNpmVersion, watch))
